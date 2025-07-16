d1441eec1914351f9697ffd38ba38154401dd293
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-16 06:38:19 +0100

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,23 +1,20 @@
-task = input("Enter your task for today: ")
+task = input("Enter your task:")
 
-priority = input("Enter the task’s priority (high, medium, low): ").lower()
+priority = input("Priority (high/medium/low):").lower()
 
-time_bound = input("Is the task time-bound? (yes or no): ").lower()
+time_bound = input("Is it time-bound? (yes/no):").lower()
 
-    match priority:
-        case "high":
-            reminder = f"The task '{task}' is a high priority task."
-        case "medium":
-            reminder = f"The task '{task}' is a medium priority task."
-        case "low":
-            reminder = f"The task '{task}' is a low priority task."
-        case _:
-            reminder = f"The task '{task}' has an unknown priority level."
+match priority:
+    case "high":
+        reminder = f"The task '{task}' is a high priority task."
+    case "medium":
+        reminder = f"The task '{task}' is a medium priority task."
+    case "low":
+        reminder = f"The task '{task}' is a low priority task."
+    case _:
+        reminder = f"The task '{task}' has an unknown priority level."
 
-    if time_bound == "yes":
-            reminder += "'Finish project report' is a high priority task that requires immediate attention today!"
-print(reminder)
+if time_bound == "yes":
+    reminder += " This task requires immediate attention today!"
 
-   if time_bond == "no":
-            reminder += "'Read a book' is a low priority task. Consider completing it when you have free time."
 print(reminder)
