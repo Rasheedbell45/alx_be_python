20c84031b9215e2f85e580c15c4b2f83e86c5272
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-29 20:02:38 +0100

    Create test_simple_calculator.py

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,43 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        """Test addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 5), 4)
+        self.assertEqual(self.calc.add(0, 0), 0)
+        self.assertEqual(self.calc.add(-4, -6), -10)
+        self.assertEqual(self.calc.add(1.5, 2.5), 4.0)
+
+    def test_subtract(self):
+        """Test subtraction method."""
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(-3, -6), 3)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+        self.assertEqual(self.calc.subtract(2.5, 1.2), 1.3)
+
+    def test_multiply(self):
+        """Test multiplication method."""
+        self.assertEqual(self.calc.multiply(3, 4), 12)
+        self.assertEqual(self.calc.multiply(-2, 5), -10)
+        self.assertEqual(self.calc.multiply(0, 10), 0)
+        self.assertEqual(self.calc.multiply(2.5, 2), 5.0)
+        self.assertEqual(self.calc.multiply(-3.0, -2), 6.0)
+
+    def test_divide(self):
+        """Test division method including edge cases."""
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(9, 3), 3)
+        self.assertEqual(self.calc.divide(7.5, 2.5), 3.0)
+        self.assertIsNone(self.calc.divide(10, 0))  # Division by zero
+        self.assertIsNone(self.calc.divide(0, 0))   # 0 divided by 0 is undefined
+        self.assertEqual(self.calc.divide(0, 5), 0)
+
+if __name__ == '__main__':
+    unittest.main()
