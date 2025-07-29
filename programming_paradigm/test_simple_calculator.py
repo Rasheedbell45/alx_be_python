ec6f0af6bcc28581368e0853d086a155a7807555
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-29 20:03:58 +0100

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,31 +7,31 @@ class TestSimpleCalculator(unittest.TestCase):
         """Set up the SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
-        """Test addition method."""
+    def test_addition(self):
+        """Test the addition method."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 5), 4)
         self.assertEqual(self.calc.add(0, 0), 0)
         self.assertEqual(self.calc.add(-4, -6), -10)
         self.assertEqual(self.calc.add(1.5, 2.5), 4.0)
 
-    def test_subtract(self):
-        """Test subtraction method."""
+    def test_subtraction(self):
+        """Test the subtraction method."""
         self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(-3, -6), 3)
         self.assertEqual(self.calc.subtract(0, 0), 0)
         self.assertEqual(self.calc.subtract(2.5, 1.2), 1.3)
 
-    def test_multiply(self):
-        """Test multiplication method."""
+    def test_multiplication(self):
+        """Test the multiplication method."""
         self.assertEqual(self.calc.multiply(3, 4), 12)
         self.assertEqual(self.calc.multiply(-2, 5), -10)
         self.assertEqual(self.calc.multiply(0, 10), 0)
         self.assertEqual(self.calc.multiply(2.5, 2), 5.0)
         self.assertEqual(self.calc.multiply(-3.0, -2), 6.0)
 
-    def test_divide(self):
-        """Test division method including edge cases."""
+    def test_division(self):
+        """Test the division method including edge cases."""
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(9, 3), 3)
         self.assertEqual(self.calc.divide(7.5, 2.5), 3.0)
