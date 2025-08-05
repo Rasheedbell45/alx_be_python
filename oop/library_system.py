2693e68d8c28c1a4d9c8b94b299a762ac9875c40
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-08-05 22:27:57 +0100

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -8,7 +8,6 @@ class Book:
     def __str__(self):
         return f"'{self.title}' by {self.author} ({self.publication_year})"
 
-
 # Derived Class: PrintBook
 class PrintBook(Book):
     def __init__(self, title, author, publication_year, page_count):
@@ -18,7 +17,6 @@ class PrintBook(Book):
     def __str__(self):
         return f"Print Book: {super().__str__()} - {self.page_count} pages"
 
-
 # Derived Class: EBook
 class EBook(Book):
     def __init__(self, title, author, publication_year, file_size_mb):
@@ -27,3 +25,20 @@ class EBook(Book):
 
     def __str__(self):
         return f"E-Book: {super().__str__()} - {self.file_size_mb}MB"
+
+
+# New Class: Library
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        if not self.books:
+            print("The library has no books.")
+        else:
+            print("Books in the Library:")
+            for book in self.books:
+                print(book)  # This will call the __str__ method of each Book
