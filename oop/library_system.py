491042db569a56d90286195bc82d4317e410cf74
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-08-05 21:56:07 +0100

    Create library_system.py

---
+++ oop/library_system.py
@@ -0,0 +1,41 @@
+# Base class
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def get_details(self):
+        return f"Book: {self.title} by {self.author}"
+
+# Derived class - EBook
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size  # in KB
+
+    def get_details(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
+# Derived class - PrintBook
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def get_details(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
+# Composition - Library class
+class Library:
+    def __init__(self):
+        self.books = []  # list of Book, EBook, or PrintBook objects
+
+    def add_book(self, book):
+        if isinstance(book, Book):
+            self.books.append(book)
+        else:
+            print("Only Book or derived types can be added to the library.")
+
+    def list_books(self):
+        for book in self.books:
+            print(book.get_details())
